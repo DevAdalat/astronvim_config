e9438b37d1ef5a546df490908b08dcb3d46b8c37
DevAdalat <akbusiness022@gmail.com>
2023-09-22 08:58:49 +0530

    Setup for flutter and Rust

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,29 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.dart" },
+  {
+    "akinsho/flutter-tools.nvim",
+    opts = {
+      debugger = {
+        enabled = true,
+        run_via_dap = true,
+      },
+      widget_guides = {
+        enabled = true,
+      },
+      lsp = {
+        color = {
+          enabled = true,
+          background = true,
+          background_color = { r = 19, g = 17, b = 24},
+          foreground = true
+        }
+      }
+    }
+  },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,5 +5,8 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
     })
+    opts.auto_install = false
+    opts.highlight.disable = {"dart"}
+    opts.ignore_install = {"dart"}
   end,
 }
